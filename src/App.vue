e64385be822815fd76f2909f8bdb16d7fd2022a9
eugene0318 <nttott@gmail.com>
2025-02-08 17:59:56 +0900

    emit

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,56 @@
 <template>
-  <div>
+  <div class="container">
     <h1>To-Do List</h1>
-    <form action=""></form>
-    <input type="text" v-model="name" />
-    <button class="btn btn-primary" @click="onSubmit">click</button>
+    <TodoSimpleForm @add-todo="addTodo" />
+
+    <div v-if="!todos.length">추가된 Todo가 없습니다.</div>
+    <TodoList :todos="todos" @toggle-todo="ToggleTodo" />
   </div>
 </template>
 <script>
 import { ref } from "vue";
+import TodoSimpleForm from "./components/TodoSimpleForm.vue";
+import TodoList from "./components/TodoList.vue";
 export default {
+  components: {
+    TodoSimpleForm,
+    TodoList,
+  },
   setup() {
-    const name = ref("kossie");
-    const onSubmit = () => {
-      console.log(name.value);
+    const toggle = ref(false);
+    const todoStyle = {
+      textDecoration: "line-through",
+      color: "gray",
+    };
+    const todos = ref([]);
+
+    const onToggle = () => {
+      toggle.value = !toggle.value;
+    };
+    const deleteTodo = index => {
+      todos.value.splice(index, 1);
+    };
+
+    const ToggleTodo = index => {
+      console.log(todos.value[index]);
+
+      todos.value[index].completed = !todos.value[index].completed;
+      console.log(todos.value[index]);
     };
     // const updateName = e => {
     //   name.value = e.target.value;
     // };
+
+    const addTodo = todo => {
+      todos.value.push(todo);
+    };
     return {
-      name,
-      onSubmit,
+      todos,
+      onToggle,
+      todoStyle,
+      deleteTodo,
+      addTodo,
+      ToggleTodo,
       //updateName,
     };
   },
@@ -27,7 +58,8 @@ export default {
 </script>
 
 <style>
-.name {
-  color: red;
+.todo {
+  color: gray;
+  text-decoration: line-through;
 }
 </style>
